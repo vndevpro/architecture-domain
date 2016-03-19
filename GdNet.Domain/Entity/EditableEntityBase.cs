47567e8c5271bb01ea0ccf0c0602d6bb01a72733
juanonsoftware <huanhvhd@gmail.com>
2016-03-19 13:29:23 +0700

    EditableEntityBase now have a new constructor to set non-default strategies

--- GdNet.Domain/Entity/EditableEntityBase.cs
+++ GdNet.Domain/Entity/EditableEntityBase.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using GdNet.Domain.Entity.Impl;
 
 namespace GdNet.Domain.Entity
 {
     public abstract class EditableEntityBase : EditableEntityBaseT<Guid>, IEditableEntity
     {
+        /// <summary>
+        /// Entity does not support state. It will be empty.
+        /// Entity's availability (IsAvailable) is false by default.
+        /// </summary>
+        protected EditableEntityBase()
+            : this(new NoStateStrategy(), new EntityNotAvailableByDefaultStrategy())
+        {
+        }
+
+        protected EditableEntityBase(IEntityStateStrategy stateStrategy, IEntityAvailabilityStrategy availabilityStrategy)
+            : base(stateStrategy, availabilityStrategy)
+        {
+        }
     }
 }
