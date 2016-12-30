4b673924f6a913ba98badf8ae63fef20cf523b38
juanonsoftware <juanonsoftware@github.com>
2016-12-30 17:53:54 +0700

    Just update GdNet.Common

--- GdNet.Domain/Repository/RepositoryBase.cs
+++ GdNet.Domain/Repository/RepositoryBase.cs
@@ -3,6 +3,10 @@ using System;
 
 namespace GdNet.Domain.Repository
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class RepositoryBase<T> : RepositoryBaseT<T, Guid>, IRepositoryBase<T>
         where T : IAggregateRoot
     {
