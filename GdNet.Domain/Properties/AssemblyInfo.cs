c71c171c979ae991c843292a89e0a6ba52e4323b
juanonsoftware <juan@vndev.top>
2017-02-22 23:36:40 +0700

    Version 2.0.4

--- GdNet.Domain/Properties/AssemblyInfo.cs
+++ GdNet.Domain/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GdNet.Domain")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Domain base interface")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("RabbitSoftware")]
 [assembly: AssemblyProduct("GdNet.Domain")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
