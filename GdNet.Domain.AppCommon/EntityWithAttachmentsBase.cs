333e5ad29b65728ffded065c3c92eea4ef2a53ba
juanonsoftware <juanonsoftware@github.com>
2017-04-11 18:09:24 +0700

    V210 GdNet.Domain.AppCommon

---
+++ GdNet.Domain.AppCommon/EntityWithAttachmentsBase.cs
@@ -0,0 +1,47 @@
+﻿using GdNet.Domain.Entity;
+using Rabbit.Foundation.List;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GdNet.Domain.AppCommon
+{
+    public abstract class EntityWithAttachmentsBase : EditableEntityBase, IEntityWithAttachments
+    {
+        private readonly IList<Attachment> _attachments;
+
+        protected EntityWithAttachmentsBase()
+        {
+            _attachments = new List<Attachment>();
+        }
+
+        public virtual ISmartList<Attachment> Attachments
+        {
+            get { return new SmartList<Attachment>(_attachments); }
+        }
+
+        public IEntityWithAttachments AddAttachment(Attachment attachment)
+        {
+            if (_attachments.All(x => x.Id != attachment.Id))
+            {
+                _attachments.Add(attachment);
+            }
+            return this;
+        }
+
+        public IEntityWithAttachments RemoveAttachment(Guid attachmentId)
+        {
+            var attachment = GetAttachment(attachmentId);
+            if (attachment != null)
+            {
+                _attachments.Remove(attachment);
+            }
+            return this;
+        }
+
+        public Attachment GetAttachment(Guid attachmentId)
+        {
+            return Attachments.FirstOrDefault(x => x.Id == attachmentId);
+        }
+    }
+}
---
+++ GdNet.Domain.AppCommon/IEntityWithAttachments.cs
@@ -0,0 +1,17 @@
+﻿using GdNet.Domain.Entity;
+using Rabbit.Foundation.List;
+using System;
+
+namespace GdNet.Domain.AppCommon
+{
+    public interface IEntityWithAttachments : IEntity
+    {
+        ISmartList<Attachment> Attachments { get; }
+
+        IEntityWithAttachments AddAttachment(Attachment attachment);
+
+        IEntityWithAttachments RemoveAttachment(Guid attachmentId);
+
+        Attachment GetAttachment(Guid attachmentId);
+    }
+}
